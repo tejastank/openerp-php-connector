9527a014c776595861b9a7b084e4a1490d23f29e
Mr. Tejas Tank <tejas.tank.mca@gmail.com>
2014-07-17 16:43:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,37 @@
 <?php
+
+// https://twitter.com/snippetbucket
+
 include_once('openerp.class.php');
 
-print "<pre/>\nOpenERP PHP connector : It support version 5 and 6.0<br/>\n Author : Tejas Tank, Tejas.tank.mca@gmail.com\n";
+print "<pre/>\nOpenERP PHP connector : It support version 6 and 7++ <br/>\n Author : Tejas Tank, Tejas.tank.mca@gmail.com\n";
 print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
 
 $rpc = new OpenERP();
 
-// $x = $rpc->login("admin", "admin", "db6edi", "http://localhost:8069/xmlrpc/");
+$x = $rpc->login("admin", "a", "mobile_client", "http://127.0.0.1:8069/xmlrpc/");
 
-$x = $rpc->login("admin", "admin", "upfdata", "http://199.195.192.3:8000/xmlrpc/");
+print ($x);
 
-print_r($x);
-echo "<textarea>user id : ", $x. "</textarea>";
+#echo $rpc->create( array('name'=>'teja22s', 'code'=> "bakbak"), "res.country");
 
-$fields = array(
-    //'id','name','model'
-);
+//echo $rpc->create( array('name'=>'teja22s', 'login'=> "bakbak"), "res.users");
+ 
+//print_r($rpc->get_fields('sale.order'));
 
-$ids = range(1,2);
-$partners = $rpc->read($ids, $fields, "res.partner");
+//print_r($rpc->get_default_values('sale.order'));
 
-print_r($partners);
+$data = $rpc->read(array(1,2), array(), "product.product");
+
+#$data = $rpc->searchread(  array(array('name','=','Service')),  "product.product");  // CORRECT
+
+//$data = $rpc->searchread(  array(array('email','!=','')),  "res.partner");  // CORRECT
+
+##$data = $rpc->read(array(1,2,3,4,5,6,7,8,9), array(), "res.users");
+##foreach($data as $p){
+##    echo "{$p[name]},{$p[phone]},{$p[email]} {$p[login]} {$p[password]}<br/>";
+##}
+
 /////print $partners = $x->unlink(array(19), "res.partner");
 
 ?>
